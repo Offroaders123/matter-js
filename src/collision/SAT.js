76afd20efdfc42458ee8d39db70361b157220da8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-10-20 21:18:47 -0700

    Collision SAT ESM

--- src/collision/SAT.js
+++ src/collision/SAT.js
@@ -12,13 +12,11 @@
 
 var SAT = {};
 
-module.exports = SAT;
+export default SAT;
 
 var Collision = require('./Collision');
 var Common = require('../core/Common');
 var deprecated = Common.deprecated;
-
-(function() {
 
     /**
      * Detect collision between two bodies using the Separating Axis Theorem.
@@ -33,5 +31,3 @@ var deprecated = Common.deprecated;
     };
 
     deprecated(SAT, 'collides', 'SAT.collides ➤ replaced by Collision.collides');
-
-})();
